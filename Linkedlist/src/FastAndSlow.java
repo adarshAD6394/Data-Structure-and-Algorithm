527854bd4f53924a0b428363a72ac6cdd39ec726
Adarsh Gupta <adarsh.gupta.21C@iitram.ac.in>
2024-12-16 09:46:22 +0530

    middle of the list using pointers

---
+++ Linkedlist/src/FastAndSlow.java
@@ -0,0 +1,35 @@
+public class FastAndSlow {
+    public static void main(String[] args) {
+        LL.Node e = new LL.Node(5, null );
+        LL.Node d = new LL.Node(4, e);
+        LL.Node c = new LL.Node(3, d);
+        LL.Node b = new LL.Node(2, c);
+        LL.Node a = new LL.Node(1, b);
+        int count = 1;
+        LL.Node t = a;
+        while(t.next != null){
+            count++;
+            t = t.next;
+        }
+        LL.Node ans = a;
+        ans = middle(a);
+        if(count % 2 == 0){
+            count = (count/2)+1;
+        }else{
+            count = (count/2)+2;
+        }
+        for (int i = 1; i <count ; i++) {
+            System.out.print(ans.val+"->");
+            ans = ans.next;
+        }
+    }
+    static LL.Node middle(LL.Node head){
+        LL.Node fast = head, slow = head;
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+}
+

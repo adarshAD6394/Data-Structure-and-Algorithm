e0c047d535f7d6b26f486e35f6cb3a1626c3f8c3
Adarsh Gupta <adarsh.gupta.21C@iitram.ac.in>
2025-01-07 18:41:09 +0530

    recover form bed and Covering Binary Tree

---
+++ Trees/src/BinarySearchTree.java
@@ -0,0 +1,45 @@
+class BinarySearchTree {
+
+    public class Node {
+        private int value;
+        private int height;
+        private Node left;
+        private Node right;
+
+        public Node(int value) {
+            this.value = value;
+        }
+
+        public int getValue() {
+            return value;
+        }
+    }
+
+    private Node root;
+
+    public BST() {
+
+    }
+    public int height(Node node){
+        if(node == null){
+            return -1;
+        }
+        return node.height;
+    }
+    public boolean isEmpty(){
+        return root == null;
+    }
+
+    public void display(){
+        display(root, "Root Node: ");
+    }
+
+    public void display(Node node, String details){
+        if(node == null){
+            return;
+        }
+        System.out.println( details+ node.value);
+        display(node.left, "left child of"+ node.getValue() + ":");
+        display(node.right, "right child of"+ node.getValue() + ":");
+    }
+}

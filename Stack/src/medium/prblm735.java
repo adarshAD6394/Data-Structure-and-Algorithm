1597a52a01103a7155a7b598d2b52c73e46b2692
Adarsh Gupta <adarsh.gupta.21C@iitram.ac.in>
2025-01-16 20:00:29 +0530

    prblm 735 Asteroid solution

--- Stack/src/medium/prblm735.java
+++ Stack/src/medium/prblm735.java
@@ -2,34 +2,40 @@ package medium;
 import java.util.*;
 public class prblm735 {
     public static void main(String[] args) {
-    int[] a = {-8,5,3,-8};
-    System.out.println(Arrays.toString(asteroidCollision(a)));
+        int[] a = {-2,-2,1,-2};
+        System.out.println(Arrays.toString(asteroidCollision(a)));
     }
+
     public static int[] asteroidCollision(int[] asteroids) {
         Stack<Integer> s = new Stack<Integer>();
-        for(int a : asteroids){
-            if(!s.isEmpty() && s.peek()<0 && a<0){
-                s.push(a);
-            }
-            else if(!s.isEmpty() && a<0){
-                int i = a;
-                while(!s.isEmpty() && a<0 && Math.abs(a)>=s.peek()){
+        for (int a : asteroids) {
+            boolean dest = false;
+            while (!s.isEmpty() && s.peek() > 0 && a < 0) {
+                if (Math.abs(a) == s.peek()) {
                     s.pop();
+                    dest = true;
+                    break;
+                } else if (Math.abs(a) > s.peek()) {
+                    s.pop();
+                } else {
+                    dest = true;
+                    break;
                 }
-                s.push(i);
-            }else{
+            }
+            if (!dest) {
                 s.push(a);
             }
         }
-        int[] ans = new int[s.size()];
-        if(ans.length == 0){
-            return ans;
-        }else {
-            for (int i = ans.length - 1; i >= 0; i--) {
-                int j = s.pop();
-                ans[i] = j;
+            int[] ans = new int[s.size()];
+            if (ans.length == 0) {
+                return ans;
+            } else {
+                for (int i = ans.length - 1; i >= 0; i--) {
+                    int j = s.pop();
+                    ans[i] = j;
+                }
+                return ans;
             }
-            return ans;
         }
-    }
+
 }

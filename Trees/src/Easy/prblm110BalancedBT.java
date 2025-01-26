e45721c92a00e87ef0d0aa697f9b49ad13cb5809
Adarsh Gupta <adarsh.gupta.21C@iitram.ac.in>
2025-01-26 23:41:46 +0530

    balanced BT prblm 110

--- Trees/src/Easy/prblm110BalancedBT.java
+++ Trees/src/Easy/prblm110BalancedBT.java
@@ -1,31 +1,30 @@
-//package Easy;
-//
-//import Trees.TreeNode;
-//
-//import static Trees.TreeNode.buildTree;
-//
-//public class prblm110BalancedBT {
-//    public static void main(String[] args) {
-//        Integer[] input = {3,9,20,null,null,15,7};
-//        TreeNode.Node root = buildTree(input);
-//        boolean ans = false;
-//        System.out.println(isBalanced(root, ans));
-//    }
-//    public static boolean isBalanced(TreeNode.Node root, boolean ans) {
-//        if (root == null) return true;
-//        int lh = 1 + isBalanced(root.left);
-//        int rh = 1 + isBalanced(root.right);
-//        if ((lh - rh) >= -1 && (lh - rh) <= 1) {
-//            return ans = true;
-//        } else {
-//            return ans = false;
-//        }
-//    }
-//        public static int isBalanced(TreeNode.Node root) {
-//            if(root == null) return 0;
-//            int lh = 1+isBalanced(root.left);
-//            int rh = 1+isBalanced(root.right);
-//                return lh-rh;
-//        }
-//    }
-//}
+package Easy;
+
+import Trees.TreeNode;
+
+import static Trees.TreeNode.buildTree;
+
+public class prblm110BalancedBT {
+    public static void main(String[] args) {
+        Integer[] input = {1,2,2,3,3,null,null,4,4};
+        TreeNode.Node root = buildTree(input);
+        System.out.println(depth(root));
+    }
+    public static boolean depth(TreeNode.Node root) {
+        return isBalanced(root) != -1;
+    }
+    public static int isBalanced(TreeNode.Node root) {
+        if(root == null) return 0;
+        int lh = isBalanced(root.left);
+        int rh = isBalanced(root.right);
+        if(lh == -1 || rh == -1){
+            return -1;
+        }
+        if(lh-rh>1 || lh-rh<-1) {
+            return -1;
+        }
+        return 1+Math.max(lh, rh);
+    }
+}
+
+

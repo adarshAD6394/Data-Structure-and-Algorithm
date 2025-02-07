e69971e5f1b920d639dbd0fac4860158482f4c29
Adarsh Gupta <adarsh.gupta.21C@iitram.ac.in>
2025-02-07 10:57:20 +0530

    prblm 257

---
+++ Trees/src/Easy/prblmBTPath.java
@@ -0,0 +1,41 @@
+package Easy;
+
+import java.util.*;
+import Trees.TreeNode;
+
+import static Trees.TreeNode.buildTree;
+import static Trees.TreeNode.printLevelOrder;
+
+public class prblmBTPath {
+    public void main(String[] args) {
+        Integer[] input = {1,2,3,null,5};
+        TreeNode.Node root = buildTree(input);
+        printLevelOrder(root);
+        System.out.println(binaryTreePaths(root));
+    }
+    List<String> ans = new ArrayList<>();
+    StringBuilder path = new StringBuilder();
+    public List<String> binaryTreePaths(TreeNode.Node root) {
+        if(root == null) return null;
+        if(root.right == null && root.left == null){
+            path.append(root.val);
+            ans.add(path.toString());
+            int n = path.length();
+            String m = ""+root.val;
+            path.delete(n-1-(m.length()-1),n);
+            return ans;
+        }else{
+            path.append(root.val).append("->");
+        }
+        if(root.left != null){
+            binaryTreePaths(root.left);
+        }
+        if(root.right != null){
+            binaryTreePaths(root.right);
+        }
+        int n = path.length();
+        String m = ""+root.val;
+        path.delete(n-1-(m.length()-1)-2,n);
+        return ans;
+    }
+}

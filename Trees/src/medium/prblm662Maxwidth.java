20bee7695e22402c9c88140d5adb9a33c3c5a8df
Adarsh Gupta <adarsh.gupta.21C@iitram.ac.in>
2025-02-11 01:44:28 +0530

    prblm 662 max width optimised

--- Trees/src/medium/prblm662Maxwidth.java
+++ Trees/src/medium/prblm662Maxwidth.java
@@ -2,32 +2,42 @@ package medium;
 
 import Trees.TreeNode;
 
-import java.util.*;
-import javafx.util.
+import java.util.AbstractMap;
+import java.util.LinkedList;
+import java.util.Queue;
 
 import static Trees.TreeNode.buildTree;
 import static Trees.TreeNode.printLevelOrder;
 
 public class prblm662Maxwidth {
     public void main(String[] args) {
-        Integer[] input = {5,3,7,3,4,6,8,2,3};
+        Integer[] input = {1,3,2,5,null,null,9,6,null,7};
         TreeNode.Node root = buildTree(input);
         printLevelOrder(root);
         System.out.println(widthOfBinaryTree(root));
     }
     public int widthOfBinaryTree(TreeNode.Node root) {
         if(root == null) return 0;
+
         int max = Integer.MIN_VALUE;
-        Queue<Pair<TreeNode.Node,Integer>> list = new LinkedList<>();
+        Queue<AbstractMap.SimpleEntry<TreeNode.Node,Integer>> list = new LinkedList<>();
+        list.offer(new AbstractMap.SimpleEntry<>(root, 0));
 
         while(!list.isEmpty()){
-            max = Math.max(list.size(), max);
-            TreeNode.Node curr = ;
-            for(int i = 0; i<list.size();i++){
-                curr = list.poll();
-                if(curr.left != null) list.offer(curr.left);
-                if(curr.right != null) list.offer(curr.right);
+            int size = list.size();
+            int left = list.peek().getValue();
+            int right = left;
+
+            for(int i = 0; i<size;i++){
+                AbstractMap.SimpleEntry<TreeNode.Node, Integer> curr = list.poll();
+                TreeNode.Node node = curr.getKey();
+                int index = curr.getValue();
+                right = index;
+
+                if(node.left != null) list.offer(new AbstractMap.SimpleEntry<>(node.left,2*index ));
+                if(node.right != null) list.offer(new AbstractMap.SimpleEntry<>(node.left,2*index+1));
             };
+            max = Math.max(max, right-left+1);
         }
         return max;
     }
